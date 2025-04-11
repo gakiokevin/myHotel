b29fb993dc1b42bcd0016a9867f055e6375066b4
kevin <gakiokevin5@gmail.com>
2025-04-11 08:52:42 +0300

    fixed env error

--- server/config/database.js
+++ server/config/database.js
@@ -2,10 +2,10 @@ import pkg from 'mariadb';
  
 
 export const pool = pkg.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'my_hotel',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   multipleStatements: true,
